794ff0faa9ee417aa2de8a0f834e187f9d2c2485
Ayushi Agarwal <84909280+ayushiagarawal1@users.noreply.github.com>
2023-12-07 10:22:55 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+// require("dotenv").config();
+
+const nextConfig = {
+  // env: {
+  //   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  //   GOOGLE_CLIENT_SECERET: process.env.GOOGLE_CLIENT_SECERET,
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: "http://localhost:3000/:path*",
+  //     },
+  //   ];
+  // },
+};
+
+module.exports = nextConfig;
